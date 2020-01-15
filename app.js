df76fb8d61d964c99b7572fdd641d52049fdf3e5
Korneliu <olbiton@gmail.com>
2020-01-14 23:38:27 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const address = process.argv[2]
+
+if (!address) {
+    console.log('Please provide and address')
+} else {
+    geocode(address, (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+            console.log(`Location: ${data.location}`)
+            console.log(`Forecast: ${forecastData}`)
+        })
+    })
+}
